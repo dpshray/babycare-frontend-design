5d21cceeb8f9a0991f41dc6bb0eddff139109ed8
Sachhyam Pokharel <pokharelsachhyam@gmail.com>
2026-02-25 17:39:32 +0545

    fix:minor fixes

--- src/config/app-constant.ts
+++ src/config/app-constant.ts
@@ -4,45 +4,48 @@ const GC_TIME = 10 * 60 * 1000;
 const PAGE_SIZE = 10;
 const MAX_PAGES_TO_SHOW = 5;
 
-
-const BABY_CARE_PHONE = "980234567";
+const BABY_CARE_PHONE1 = "9802334171";
+const BABY_CARE_PHONE2 = "9705477368";
 const BABY_CARE_EMAIL = "babycare@gmail.com";
-const BABY_CARE_ADDRESS = "Kathmandu, Nepal";
+const BABY_CARE_ADDRESS = "Lokanthali, Bhaktapur";
 const BABY_CARE_ADDRESS_URL = "https://www.google.com/maps/place/Kathmandu";
 const BABY_CARE_APP_NAME = "BabyCare";
 const BABY_CARE_APP_URL = "https://babycare.com";
-const BABY_CARE_PLAY_STORE_URL = "https://play.google.com/store/apps/details?id=com.dwork.babycare";
-const BABY_CARE_APP_STORE_URL = "https://apps.apple.com/us/app/babycare/id1234567890";
-const BABY_CARE_APP_DESCRIPTION = "BabyCare is a platform for parents to find the best products and services for their babies.";
-const BABY_CARE_APP_KEYWORDS = "baby care, baby products, baby services, baby health, baby nutrition, baby health care, baby health tips, baby health advice, baby health care, baby health care, baby health care";
+const BABY_CARE_PLAY_STORE_URL =
+  "https://play.google.com/store/apps/details?id=com.dwork.babycare";
+const BABY_CARE_APP_STORE_URL =
+  "https://apps.apple.com/us/app/babycare/id1234567890";
+const BABY_CARE_APP_DESCRIPTION =
+  "BabyCare is a platform for parents to find the best products and services for their babies.";
+const BABY_CARE_APP_KEYWORDS =
+  "baby care, baby products, baby services, baby health, baby nutrition, baby health care, baby health tips, baby health advice, baby health care, baby health care, baby health care";
 
+const BABY_CARE_FACEBOOK_URL = "https://www.facebook.com/thebabycareapp";
+const BABY_CARE_INSTAGRAM_URL = "https://www.instagram.com/thebabycareapp";
+const BABY_CARE_TWITTER_URL = "https://twitter.com/babycare";
+const BABY_CARE_LINKEDIN_URL = "https://www.linkedin.com/company/babycare";
 
-const BABY_CARE_FACEBOOK_URL = 'https://www.facebook.com/thebabycareapp'
-const BABY_CARE_INSTAGRAM_URL = 'https://www.instagram.com/thebabycareapp'
-const BABY_CARE_TWITTER_URL = 'https://twitter.com/babycare'
-const BABY_CARE_LINKEDIN_URL = 'https://www.linkedin.com/company/babycare'
-
-
-const GOOGLE_CLIENT_ID = "1093076365245-ta2sb81v6qj6ruocdilnr8vpkm2k741g.apps.googleusercontent.com"
+const GOOGLE_CLIENT_ID =
+  "1093076365245-ta2sb81v6qj6ruocdilnr8vpkm2k741g.apps.googleusercontent.com";
 // const GOOGLE_CLIENT_ID = "432701077563-8qqdd9ojv0tnqknhov2vcov8i6jfgqt7.apps.googleusercontent.com"
 
-export
-{
-    QUERY_STALE_TIME,
-    PAGE_SIZE,
-    GC_TIME,
-    BABY_CARE_PHONE,
-    BABY_CARE_EMAIL,
-    BABY_CARE_ADDRESS,
-    BABY_CARE_APP_NAME,
-    BABY_CARE_APP_URL,
-    BABY_CARE_PLAY_STORE_URL,
-    BABY_CARE_APP_STORE_URL,
-    BABY_CARE_APP_DESCRIPTION,
-    BABY_CARE_APP_KEYWORDS,
-    BABY_CARE_FACEBOOK_URL,
-    BABY_CARE_INSTAGRAM_URL,
-    BABY_CARE_TWITTER_URL,
-    BABY_CARE_LINKEDIN_URL,
-    GOOGLE_CLIENT_ID
-}
+export {
+  QUERY_STALE_TIME,
+  PAGE_SIZE,
+  GC_TIME,
+  BABY_CARE_PHONE1,
+  BABY_CARE_PHONE2,
+  BABY_CARE_EMAIL,
+  BABY_CARE_ADDRESS,
+  BABY_CARE_APP_NAME,
+  BABY_CARE_APP_URL,
+  BABY_CARE_PLAY_STORE_URL,
+  BABY_CARE_APP_STORE_URL,
+  BABY_CARE_APP_DESCRIPTION,
+  BABY_CARE_APP_KEYWORDS,
+  BABY_CARE_FACEBOOK_URL,
+  BABY_CARE_INSTAGRAM_URL,
+  BABY_CARE_TWITTER_URL,
+  BABY_CARE_LINKEDIN_URL,
+  GOOGLE_CLIENT_ID,
+};
